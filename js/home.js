887fdefb3086c2a42c17918ad2962f5951f096b9
Hansini Chetan Dhameliya <hansini.dhameliya2@gmail.com>
2026-03-02 16:55:48 -0500

    Add Advanced Functionality
    This commit adds advanced functionality to the counter.

--- js/home.js
+++ js/home.js
@@ -1,7 +1,9 @@
 import { mobileMenu } from "./modules/mobile-menu.js";
 import { Counter } from "./modules/counter.js";
+import { StepCounter } from "./modules/counter.js";
 
 mobileMenu();
 const counter1 = new Counter("#counter-container1", 10);
 const counter2 = new Counter("#counter-container2");
 const counter3 = new Counter("#counter-container3");
+const counter4 = new StepCounter("#counter-container4", 10, 5);
